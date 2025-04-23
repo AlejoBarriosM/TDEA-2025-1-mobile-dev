6072bf9cd5b2791b6c3d5f5ff1d96e608f6e5bf7
ShadowReco <shadowbloodinfinite@gmail.com>
2025-04-23 00:06:24 -0500

    Se corrige error en App.js

--- App.js
+++ App.js
@@ -4,27 +4,20 @@ import { NavigationContainer } from "@react-navigation/native";
 import MainStackNavigator from "./Navigation/MainStackNavigator";
 import { UsersProvider } from "./context/UsersContext";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { CompletedExercisesProvider } from './screens/Workout/subscreens/Subscreensroutine/Session1';
 
-const Stack = createStackNavigator();
-
-const App = () => {
+export default function App() {
   return (
-    <CompletedExercisesProvider>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <UsersProvider>
-          <NavigationContainer>
-            <SafeAreaView style={styles.container}>
-              <MainStackNavigator />
-            </SafeAreaView>
-          </NavigationContainer>
-        </UsersProvider>
-      </GestureHandlerRootView>
-    </CompletedExercisesProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <UsersProvider>
+        <NavigationContainer>
+          <SafeAreaView style={styles.container}>
+            <MainStackNavigator />
+          </SafeAreaView>
+        </NavigationContainer>
+      </UsersProvider>
+    </GestureHandlerRootView>
   );
-};
-
-export default App;
+}
 
 const styles = StyleSheet.create({
   container: {
